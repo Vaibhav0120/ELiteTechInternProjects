c71d7ea0bb52af8e933515838ed593dd3cc3c176
Vaibhav Sharma <vaibhavsh0120@gmail.com>
2024-10-03 22:23:22 +0530

    Create Acronyms-Creator.py

---
+++ Acronyms-Creator.py
@@ -0,0 +1,21 @@
+def create_acronym(phrase):
+    words = phrase.split()
+    
+    acronym = ''.join([word[0].upper() for word in words])
+    
+    return acronym
+
+def acronym_creator():
+    while True:
+        phrase = input("Enter a phrase to create an acronym: ")
+
+        acronym = create_acronym(phrase)
+
+        print(f"The acronym for '{phrase}' is: {acronym}")
+        
+        use_again = input("\nDo you want to create another acronym? (yes/y or no): ").lower()
+        if use_again not in ['yes', 'y']:
+            print("Thanks for using the Acronym Creator! Goodbye!")
+            break
+
+acronym_creator()
