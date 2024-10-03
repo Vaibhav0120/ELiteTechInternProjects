31080868dddec25998dbcd4bd4292f00d33d4020
Vaibhav Sharma <vaibhavsh0120@gmail.com>
2024-10-03 22:16:04 +0530

    Create Dice-Roll.py

---
+++ Dice-Roll.py
@@ -0,0 +1,16 @@
+import random
+
+def roll_dice():
+    print("Welcome to the Dice Roll Simulator!")
+    
+    while True:
+        dice_roll = random.randint(1, 6)
+        print(f"\nYou rolled a {dice_roll}!")
+
+        roll_again = input("\nDo you want to roll the dice again? (yes/no): ").lower()
+
+        if roll_again not in ['yes', 'y']:
+            print("Thanks for playing! Goodbye!")
+            break
+
+roll_dice()
